f6bccf807f0bea5337f71d2d3933ab8a9064043d
supermilllka <milka.87@bk.ru>
2022-12-21 18:11:02 +0400

    Добавлены изменения в Task09

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -5,7 +5,7 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
-Console.Write("Введите первое число: ");
+      Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
--- Task09/Program.cs
+++ Task09/Program.cs
@@ -7,8 +7,8 @@
 
 int number = new Random().Next(10,100);
 Console.WriteLine($"Случайное число из отрезка 10-99 => {number}");
- int firstDigit = number/10;
- int secondDigit = number%10;
+ //int firstDigit = number/10;
+ //int secondDigit = number%10;
 
  //if (firstDigit>secondDigit) 
 //{
@@ -20,6 +20,24 @@ Console.WriteLine($"Случайное число из отрезка 10-99 => {
  //}
 
  
- int result = firstDigit>secondDigit ? firstDigit : secondDigit;
- Console.WriteLine($"Наибольшая цифра числа => {result}");
+ //int result = firstDigit>secondDigit ? firstDigit : secondDigit;
+ //Console.WriteLine($"Наибольшая цифра числа => {result}");
 
+ int maxDigit = MaxDigit(number);
+  Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
+
+  int maxDigit1 = MaxDigit(45);
+  Console.WriteLine($"Наибольшая цифра числа => {maxDigit1}");
+
+  int maxDigit2 = MaxDigit(78);
+  Console.WriteLine($"Наибольшая цифра числа => {maxDigit2}");
+
+int MaxDigit(int num) //num=number
+{
+   int firstDigit = num/10;
+ int secondDigit = num%10; 
+ int result = firstDigit>secondDigit ? firstDigit : secondDigit;
+  return result;
+
+}
+;
