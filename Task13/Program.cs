7b9c117df41ce6171a6c479fd87f52ade4969baa
supermilllka <milka.87@bk.ru>
2022-12-23 15:03:59 +0400

    Домашняя задача №13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -7,18 +7,14 @@
 Console.WriteLine("Введите  число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+if (number<0)
+{
+  number = number*(-1);
+}
+
 if (number>=100)
 {
 int thirdDigit = ThirdDigit(number);
-int ThirdDigit (int num)
-
-{
-  while (num>999)
-  {
-    num=num/10;
-  }
-  return num % 10;
-}
 Console.WriteLine(thirdDigit);
 }
 else
@@ -26,3 +22,11 @@ else
    Console.Write("Третьей цифры нет"); 
 }
 
+int ThirdDigit (int num)
+{
+  while (num>999)
+  {
+    num=num/10;
+  }
+  return num % 10;
+}
