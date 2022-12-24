0c0c393954363c3ac606c78939abd0ffe62eb3e1
supermilllka <milka.87@bk.ru>
2022-12-24 20:13:38 +0400

    Домашняя задача №23

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+5 ->
+1 | 1
+2 | 8
+3 | 27
+4 | 64
+5 | 125*/
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintCube(number);
+
+void PrintCube(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index}  |   {index * index * index}");
+        index++;
+    }
+}
