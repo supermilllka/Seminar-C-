c9e845011e54033121fd78620ce4c8dfe2723b09
supermilllka <milka.87@bk.ru>
2023-01-12 23:37:11 +0400

    добавить новые задачи

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -8,19 +8,15 @@
 Console.WriteLine("Введите  трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number>=100 && number <=999)
+if (number >= 100 && number <= 999)
 {
+    int firstDigit = number / 10;
 
-int firstDigit = number/10;
+    int secondDigit = firstDigit % 10;
 
-int secondDigit= firstDigit%10;
-
-Console.Write($" вторая  цифра:{secondDigit}");
-
+    Console.Write($" вторая  цифра:{secondDigit}");
 }
 else
 {
-Console.Write("Вы ввели не корректное число");
+    Console.Write("Вы ввели не корректное число");
 }
-
-
--- Task21/Program.cs
+++ Task21/Program.cs
@@ -20,11 +20,14 @@ Console.WriteLine("ZB : ");
 int zb = Convert.ToInt32(Console.ReadLine());
 
 double ourDistance = Math.Round(Distance(xa, xb, ya, yb, za, zb), 2, MidpointRounding.ToZero);
-Console.WriteLine($"Расстояние между точками с координатами {xa} , {xb} , {ya}, {yb}  и {za}, {zb} равно {ourDistance}.");
+Console.WriteLine(
+    $"Расстояние между точками с координатами {xa} , {xb} , {ya}, {yb}  и {za}, {zb} равно {ourDistance}."
+);
 
-
-double Distance (int x1, int x2, int y1, int y2, int z1, int z2)
+double Distance(int x1, int x2, int y1, int y2, int z1, int z2)
 {
-    double distance = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2));
+    double distance = Math.Sqrt(
+        Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2)
+    );
     return distance;
 }
