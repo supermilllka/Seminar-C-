75418e6c8ed7662b58645ab2707a851012a69306
supermilllka <milka.87@bk.ru>
2023-01-15 11:03:35 +0400

    Домашняя задача №25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -8,23 +8,23 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine(NaturalNumber(number2) ? " " : "Ошибка. Число В должно быть натуральным");
+
 int printPower = PrintPower(number1, number2);
-Console.WriteLine($"число {number1} в натуральной степени {number2} -> {printPower}");
+Console.WriteLine($"число {number1} в натуральной степени {number2} -> {printPower}");// не понимаю как мне не выводить эту строчку, если степень отрицательная
+
+bool NaturalNumber(int num2)
+{
+    return num2 > 0;
+}
 
 int PrintPower(int num1, int num2)
 {
     int result = 1;
 
-    if (num2 < 0)
+    for (int i = 1; i <= num2; i++)
     {
-        Console.WriteLine("Ошибка. Число В должно быть натуральным");
-    }
-    else
-    {
-        for (int i = 1; i <= num2; i++)
-        {
-            result = result * num1;
-        }
+        result = result * num1;
     }
     return result;
 }
