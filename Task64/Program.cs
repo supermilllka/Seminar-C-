4d2c9cd0bd75d20b049e9102371109ab4ccbb218
supermilllka <milka.87@bk.ru>
2023-02-04 13:59:06 +0400

    Task64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -2,3 +2,21 @@
 натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 0)
+
+    NaturalNumbers(number);
+else
+    Console.Write("Введенное число не является натуральным");
+
+void NaturalNumbers(int num)
+{
+    if (num == 0)
+        return;
+
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+}
