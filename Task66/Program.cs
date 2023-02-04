70770da4079fb3c7bbc0d747f3e6f5b326f0ff10
supermilllka <milka.87@bk.ru>
2023-02-04 15:01:45 +0400

    Task68

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -14,6 +14,7 @@ Console.WriteLine(sumDigits);
 }
 else
 Console.WriteLine("Введенные числа должны быть натуральными");
+
 int SumDigits(int first, int second)
 {
     if (first > second)
--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,3 +1,27 @@
 ﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью
 рекурсии. Даны два неотрицательных числа m и n.
 m = 3, n = 2 -> A(m,n) = 29*/
+
+Console.WriteLine("Введите первое неотрицательное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе неотрицательное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (M >=0 && N >=0)
+{
+int akkerman = Akkerman(M, N);
+Console.WriteLine(akkerman);
+}
+else
+Console.WriteLine("Введенные числа должны быть неотрицательными");
+
+int Akkerman(int first, int second)
+{
+    if (first == 0)
+        return second + 1;
+    if (first != 0 && second == 0)
+        return Akkerman(first - 1, 1);
+    if (first > 0 && second > 0)
+        return Akkerman(first - 1, Akkerman(first, second - 1));
+    return Akkerman(first, second);
+}
